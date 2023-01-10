23551aed0ef7931d220aaf97d3414036d5ea5975
Daniel Lima <danielrlima2015@gmail.com>
2023-01-10 18:11:05 -0300

    feat: Modeling the application domain

---
+++ src/Domain/Entity/Content.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Entity;
+
+use DomainException;
+
+class Content
+{
+    private string $content;
+
+    public function __construct(string $content)
+    {
+        if (!$this->isValidContentLength($content)) {
+            throw new DomainException('Content length error.');
+        }
+
+        $this->content = $content;
+    }
+
+    public function __toString(): string
+    {
+        return $this->content;
+    }
+
+    private function isValidContentLength(string $content): bool
+    {
+        return strlen($content) >= 5 && strlen($content) <= 248;
+    }
+}
---
+++ src/Domain/Entity/Notification.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Entity;
+
+use Symfony\Component\Uid\Uuid;
+
+class Notification
+{
+    public function __construct(
+        private Uuid $id,
+        private string $recipientId,
+        private Content $content,
+        private string $category,
+        private ?\DateTimeInterface $readAt,
+        private ?\DateTimeInterface $canceledAt,
+        private \DateTimeImmutable $createdAt
+    ) {
+    }
+
+    public function id(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function recipientId(): string
+    {
+        return $this->recipientId;
+    }
+
+    public function setRecipientId(string $recipientId): void
+    {
+        $this->recipientId = $recipientId;
+    }
+
+    public function content(): Content
+    {
+        return $this->content;
+    }
+
+    public function setContent(Content $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function category(): string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): void
+    {
+        $this->category = $category;
+    }
+
+    public function readAt(): ?\DateTimeInterface
+    {
+        return $this->readAt;
+    }
+
+    public function read(): void
+    {
+        $this->readAt = new \DateTime();
+    }
+
+    public function unread(): void
+    {
+        $this->readAt = null;
+    }
+
+    public function canceledAt(): ?\DateTimeInterface
+    {
+        return $this->canceledAt;
+    }
+
+    public function cancel(): void
+    {
+        $this->canceledAt = new \DateTime();
+    }
+
+    public function createdAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+}
